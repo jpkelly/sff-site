95701eb5e1f9b1d16947d2a5d9320f066e327920
JP Kelly <jp@jpkvideo.com>
2020-05-19 21:15:39 -0700

    Updates

---
+++ precache-manifest.5f8f9d6ba872955e9d10ac13b496428c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f2ed4695fbffefed882ce3035bc635",
+    "url": "/sff-site/index.html"
+  },
+  {
+    "revision": "7144897218d360c8b497",
+    "url": "/sff-site/static/css/main.2eaf6942.chunk.css"
+  },
+  {
+    "revision": "086a8c62dd68a6649282",
+    "url": "/sff-site/static/js/2.7d055290.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/sff-site/static/js/2.7d055290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7144897218d360c8b497",
+    "url": "/sff-site/static/js/main.63b15fa6.chunk.js"
+  },
+  {
+    "revision": "401a1a554f9ce18b4f6b",
+    "url": "/sff-site/static/js/runtime-main.f177642b.js"
+  },
+  {
+    "revision": "e2d6f06e1120893e7f98f8f5023e9275",
+    "url": "/sff-site/static/media/sfflogo.e2d6f06e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sff-site/precache-manifest.db2223195defce3a00343ba5968f1fba.js"
+  "/sff-site/precache-manifest.5f8f9d6ba872955e9d10ac13b496428c.js"
 );
 
 self.addEventListener('message', (event) => {
